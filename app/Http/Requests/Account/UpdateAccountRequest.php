bd9b276072a8d3738ad8de18a7d0499f7d9e825c
SergeyMoroz <sergey.moroz@artpix3d.com>
2022-04-03 11:07:24 +0300

    create account page

--- app/Http/Requests/Account/UpdateAccountRequest.php
+++ app/Http/Requests/Account/UpdateAccountRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Account;
 
+use App\Rules\PhoneRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateAccountRequest extends FormRequest
@@ -28,7 +29,7 @@ class UpdateAccountRequest extends FormRequest
             'surname' => ['string', 'min:3', 'max:50'],
             'email' => ['email', 'unique:users,email,' . auth()->user()->id],
             'birthdate' => ['date'],
-            'phone' => ['string', 'max:15', 'unique:users,email,' . auth()->user()->id],
+            'phone' => ['string', 'max:15', new PhoneRule(), 'unique:users,email,' . auth()->user()->id],
             'balance' => ['numeric'],
         ];
     }
