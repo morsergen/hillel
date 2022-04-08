e5845b35c638b5cf840714f7cc8af503fa30bf79
SergeyMoroz <sergey.moroz@artpix3d.com>
2022-04-08 18:39:16 +0300

    create notification about update product

---
+++ app/Notifications/ProductUpdateNotification.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Product;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ProductUpdateNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * @var Product
+     */
+    private Product $product;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        //mail, database, broadcast, nexmo и slack
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->greeting($notifiable->full_name)
+            ->line('The product ' . $this->product->title . ' from you wish list')
+            ->line('was update and already exists in our shop')
+            ->action('Visit product page', route('products.show', $this->product))
+            ->line('Thank you!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
