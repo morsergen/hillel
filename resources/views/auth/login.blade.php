bfaa1dd39bba08a1300afe1e2848e301b2cf0a43
SergeyMoroz <sergey.moroz@artpix3d.com>
2022-04-28 18:38:41 +0300

    add recaptcha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Loginfghfghfgh') }}</div>
+                <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
